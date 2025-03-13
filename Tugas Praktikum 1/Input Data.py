d5ad2fe97297af4f62b375da63a4fd363456225e
Weiss_GCM <darma20060311@gmail.com>
2025-03-13 20:18:31 +0700

    Moved Input Data.py

---
+++ Tugas Praktikum 1/Input Data.py
@@ -0,0 +1,15 @@
+# Studi Kasus: Membuat demonstrasi input, lalu menampilkan outputnya. Menggunakan contoh sederhana menanyakan nama dan barang yang dimiliki lalu akan menampilkan hasilnya pada outuput.
+
+# Input
+user = input(str('Nama Anda : '))
+barang1 = input(str('Masukan barang 1 : '))
+barang2 = input(str('Masukan barang 2 : '))
+barang3 = input(str('Masukan barang 3 : '))
+
+# Output
+print('halo',user)
+print('Anda memiliki barang berupa: ')
+print('-',barang1)
+print('-',barang2)
+print('-',barang3)
+
